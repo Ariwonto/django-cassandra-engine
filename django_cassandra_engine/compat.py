0e5e5f102780b0ecd6640b605d86b2803691b657
Carlos Tovar <carlos.tovar@ariwonto.com>
2020-10-10 16:37:16 -0500

    support ConsistencyLevel on Django

--- django_cassandra_engine/compat.py
+++ django_cassandra_engine/compat.py
@@ -10,6 +10,7 @@ try:
         ModelMetaClass, ModelException, ColumnDescriptor,
         ModelDefinitionException, BaseModel, Model
     )
+    from dse.cqlengine.statements import SelectStatement
     from dse.util import OrderedDict
 except ImportError:
     try:
@@ -23,6 +24,7 @@ except ImportError:
             ModelDefinitionException, BaseModel, Model
         )
         from cassandra.util import OrderedDict
+        from cassandra.cqlengine.statements import SelectStatement
     except ImportError:
             raise ImportError('You must install either dse-driver or '
                               + 'cassandra-driver!')
